451b2cd94d674e3c9d0d921b54b1b4a066a9d10d
hashimmalik59 <hm3630450@gmail.com>
2024-10-02 07:59:17 +0500

    Exercise questions

--- 01_basics/05_comparisonDatatype.js
+++ 01_basics/05_comparisonDatatype.js
@@ -10,4 +10,26 @@
 // console.log(null !== 0);
 
 // ===
-console.log(3 === 4 - 1);
+// console.log(3 === 4 - 1);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// What is the difference between == and === in JavaScript when comparing two values?
+
+// What will be the output of the following comparison: 0 == false and 0 === false? Explain why.
+
+// Given the values null and undefined, what will be the result of the comparison null == undefined and null === undefined?
+
+// What is the result of the comparison "10" > 5? Explain the behavior of JavaScript in this case.
+
+// In JavaScript, what is the result of comparing an empty array [] and false using both == and ===? Why do these comparisons yield different results?
+
+// What will be the result of comparing two objects {} and {} using both == and ===? Why is this the case?
+
+// Explain the result of the following comparison: NaN === NaN. Why is the behavior of NaN unique in JavaScript comparisons?
+
+// What is the result of the following comparison: null > 0 and null == 0? Explain how JavaScript handles null in comparisons.
+
+// Given the array [1, 2], what will be the result of the comparison [1, 2] == "1,2"? Why?
+
+// What will be the result of the following comparison: true == 1 and true === 1? Explain the behavior of JavaScript in these comparisons.
