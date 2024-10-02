15534c3fcb2be244783faea0b23c0497ffd4e927
hashimmalik59 <hm3630450@gmail.com>
2024-10-02 07:22:39 +0500

    Comparison of Datatype

---
+++ 01_basics/05_comparisonDatatype.js
@@ -0,0 +1,13 @@
+// console.log(3 > 1);
+
+// console.log(null < 0);
+// console.log(null > 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null != 0);
+// console.log(null === 0);
+// console.log(null !== 0);
+
+// ===
+console.log(3 === 4 - 1);
