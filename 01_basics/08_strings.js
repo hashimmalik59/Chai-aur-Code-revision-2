5db7f2f35f25e32b064d55bd231534a6cc2f4276
hashimmalik59 <hm3630450@gmail.com>
2024-10-06 10:02:47 +0500

    exercose

--- 01_basics/08_strings.js
+++ 01_basics/08_strings.js
@@ -23,46 +23,46 @@ let fullName = ("Hashim Ahmad Malik")
 
 // Question 1: How can you convert the string "JavaScript is fun" to uppercase?
 let message = "Javascript is fun"
-console.log(message.toUpperCase());
+// console.log(message.toUpperCase());
 
 // Question 2: Given the string "Hello, World!", write a method to extract the word "World" from it.
 let hello = "Hello, World"
-console.log(hello.substring(7, 12));
+// console.log(hello.substring(7, 12));
 
 // Question 3: How can you check if the string "Learn JavaScript" contains the word "JavaScript"?
 let str = "Learn Javascript"
-console.log(str.includes("Javascript"));
+// console.log(str.includes("Javascript"));
 
 // Question 4: Given the string "Frontend" and the string "Engineer", concatenate both strings with a space in between.
 let str1 = "Frontend "
 let str2 = "Engineer"
-console.log(str1 + str2);
+// console.log(str1 + str2);
 
 // Question 5: Write a method to replace all occurrences of the word "JS" with "JavaScript" in the string "I love JS because JS is awesome".
 let messageJS = "I love JS because JS is awesome"
-console.log(messageJS);
-console.log(messageJS.replace("JS", "Javascript"));
+// console.log(messageJS);
+// console.log(messageJS.replace("JS", "Javascript"));
 
 // Question 6: How can you trim the whitespace from the beginning and end of the string " Hello JS! "?
 let greeting = "  Hello JS     "
-console.log(greeting);
-console.log(greeting.trim());
+// console.log(greeting);
+// console.log(greeting.trim());
 
 // Question 7: Given the string "apple,banana,cherry", split this string into an array of words.
 let fruit = "apple"
 let fruit2 = "banana"
 let fruit3 = "cherry"
-console.log(fruit.split(fruit2, fruit3));
+// console.log(fruit.split(fruit2, fruit3));
 
 // Question 8: Write a method to reverse the string "coding".
 let code = "coding"
-console.log(code.reverse());
+// console.log(code.reverse());
 
 // Question 9: How can you get the character at index 4 in the string "JavaScript"?
 let js = "Javascript"
-console.log(js[4]);
+// console.log(js[4]);
 
 // Question 10: How can you convert the string "12345" into a number?
 let str3 = "12345"
 let intoNumber = Number(str3)
-console.log(intoNumber);
+// console.log(intoNumber);
