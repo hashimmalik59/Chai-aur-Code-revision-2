0d087baace74f44f576128fc44ec45312a455544
hashimmalik59 <hm3630450@gmail.com>
2024-10-08 08:07:05 +0500

    Array part1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,25 @@
+let myData = ["Hashim", 23, true] // sqaure bracket is called an array and allow all types of array
+// console.log(myData);
+
+// myData.push(false) // adding an element/elements at the end of an array
+// console.log(myData);
+// myData.pop() // removing an element at the end of an array
+// console.log(myData);
+
+// myData.unshift('Malik') // adding an element/elements at the start of an array
+// console.log(myData);
+// myData.shift() // removing an element at the start of an array
+// console.log(myData);
+
+// console.log(myData.includes("Hashim")); // it will check is this value is exist in this array (answer is always true of false)
+// console.log(myData.indexOf(23)); // it will check our elements exist on which index in the array (answer is always true of false)
+
+// console.log(myData.join()); // it will join our array like a string
+// console.log(typeof myData.join()); // String
+
+// console.log(myData.slice(0, 2)); // it will returns a piece of slice (Note: last index is not returns)
+
+// let newArr = myData.splice(0, 10, "xxx") // it will add delete replace an array (start, delete, replace)
+// console.log(newArr);
+// console.log(myData.splice(2, 1, "xxx"))
+// console.log(myData.length);
