b05bfff5c082b7f1cf91b1eff9c93943c90583de
hashimmalik59 <hm3630450@gmail.com>
2024-10-14 20:54:20 +0500

    Function with objects and Function with arrays

---
+++ 03_basics/02_functionObjectArray.js
@@ -0,0 +1,26 @@
+// function calculateCartPrice(num1, num2, num3, ...value) { // rest operator used for remaining values
+// return value
+// }
+// console.log(calculateCartPrice(2, 34, 688, 1567))
+
+// let myData = {
+//     name: 'Hashim',
+//     age: 23,
+// }
+
+// function myObject(user) {
+//     console.log(`My name is ${user.name} and my age is ${user.age}`);
+// }
+
+// myObject(myData)
+// myObject({
+//     name: "Ahmad",
+//     age: 18
+// })
+
+// let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// function myArray(arr) {
+    // return arr[1]
+// }
+// console.log(myArray(numbers));
+// console.log(myArray(["Hashim", "Ahmad", "Malik"]));
