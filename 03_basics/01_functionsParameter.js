c68a78ccc6cac79d8546c3b2879b3b7a9afe4573
hashimmalik59 <hm3630450@gmail.com>
2024-10-14 20:24:22 +0500

    Functions and Parameter

---
+++ 03_basics/01_functionsParameter.js
@@ -0,0 +1,9 @@
+// function myName() { // function
+//     console.log("Hashim");
+// }
+// myName()
+                  // Parameters
+// function addTwoNums(num1, num2){
+    // console.log(num1, num2);   
+// }      // Argument
+// addTwoNums(3,6)
