0c3ac6b1627045173d42d7432cb790b824a00c2e
hashimmalik59 <hm3630450@gmail.com>
2024-10-22 17:13:46 +0500

    While and Do while loop

---
+++ 05_loops/02_whileDoLoop.js
@@ -0,0 +1,22 @@
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++
+// }
+
+// let name = ["Hashim", "Ahmad", "Malik"]
+
+// let arr = 0
+// while (arr < name.length) {
+//     console.log(name[arr]);
+//     arr++
+// }
+
+
+// Do while loop(it will always executes at least one time)
+
+// let j = 15
+// do {
+//     console.log(j);
+//     j++
+// } while (j <= 10)
