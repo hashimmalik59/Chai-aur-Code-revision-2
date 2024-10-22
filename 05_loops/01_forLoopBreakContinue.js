eab12c5024648f5048395446de4f7677c18f5a70
hashimmalik59 <hm3630450@gmail.com>
2024-10-22 17:13:20 +0500

    For loop with beark and continue

---
+++ 05_loops/01_forLoopBreakContinue.js
@@ -0,0 +1,20 @@
+/* Initialization, Condition and Upadation */
+for (let i = 0; i < 10; i++) {
+    if (i === 4) {
+        // console.log(`4 is my Lucky number`);
+    }
+    // console.log(i);
+}
+
+let myArray = ["Superman", "Batman", "Flashman"]
+for (let i = 0; i < myArray.length; i++) {
+    // console.log(myArray[i]);
+}
+
+
+for (let index = 0; index <= 10; index++) {
+    if (index === 4) {
+        console.log(`${index} lucky number now stop loop`);
+    }
+    console.log(index);
+}
